2d2f30c78e277f4fa902400e8c9897a56c888308
Alicia Zaludova <aliciazaludova@gmail.com>
2018-04-16 13:49:33 -0500

    cards printing + styled

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,22 +1,29 @@
 // writes to the dom
 
-const printToDom = (petArray, divId) => {
-  document.getElementById(divId).innerHTML = petArray;
+const outputDiv = document.getElementById('pets');
+
+const printToDom = (petArray) => {
+  outputDiv.innerHTML = buildDomString(petArray);
 };
 
 const buildDomString = (petArray) => {
-  console.log('pets');
   let strang = '';
+  strang += `<div class="container">`;
+  strang += `<div class="row">`;
   petArray.forEach(pet => {
-    strang += `<div class="pet-card">`;
-    strang += `<img src="${pet.imageUrl}">`;
+    strang += `<div class="col-sm-4">`;
+    strang += `<div class="pet-card panel panel-default">`;
+    strang += `<img class="pet-image" src="${pet.imageUrl}">`;
     strang += `<h2>${pet.name}</h2>`;
     strang += `<p>${pet.color}</p>`;
     strang += `<p>${pet.specialSkill}</p>`;
     strang += `<p>${pet.type}</p>`;
     strang += `</div>`;
+    strang += `</div>`;
   });
-  printToDom(strang, 'pets');
+  strang += `</div>`;
+  strang += `</div>`;
+  return strang;
 };
 
-module.exports = buildDomString;
+module.exports = printToDom;
